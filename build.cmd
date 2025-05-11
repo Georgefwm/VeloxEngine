84de3f536745f5ac93bfa3b98c852cdc66843cd3
George Mclachlan <georgemclachlan2@gmail.com>
2025-05-11 15:45:40 +0100

    Misc project changes

---
+++ build.cmd
@@ -0,0 +1,13 @@
+:: Compile shaders
+:: glslc (glsl_shader_file_name) -o (output_shader_name)
+:: if %errorlevel% neq 0 exit /b 1
+:: exit on fail
+
+mkdir "build"
+cd "build"
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -G Ninja ..
+cd ..
+:: exit on fail
+
+ninja
+:: exit on fail
