6e5b5584ec1ba2c34be4b2c131e707c711800544
George Mclachlan <georgemclachlan2@gmail.com>
2025-05-13 01:06:55 +0100

    Get ImGui working (renderer to be improved)

--- Velox/src/Velox.cpp
+++ Velox/src/Velox.cpp
@@ -3,10 +3,16 @@
 #include <SDL3/SDL.h>
 #include "SDL3/SDL_events.h"
 
+#include <imgui.h>
+#include <imgui_impl_sdl3.h>
+#include <imgui_impl_sdlrenderer3.h>
+
 #include <cstdio>
 
 bool g_quitRequested = false;
-SDL_Window* g_window = nullptr;
+
+SDL_Window*   g_window   = nullptr;
+SDL_Renderer* g_renderer = nullptr;
 
 void Velox::test()
 {
@@ -24,6 +30,23 @@ void Velox::test()
         return;
     }
 
+    g_renderer = SDL_CreateRenderer(g_window, nullptr);
+    SDL_SetRenderVSync(g_renderer, 1);
+
+    if (!g_renderer) {
+        printf("Error: SDL_CreateRenderer(): %s\n", SDL_GetError());
+        return;
+    }
+
+    SDL_ShowWindow(g_window);
+    
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+
+    ImGui_ImplSDL3_InitForSDLRenderer(g_window, g_renderer);
+    ImGui_ImplSDLRenderer3_Init(g_renderer);
+
     // Main loop.
     while (!g_quitRequested)
     {
@@ -31,6 +54,8 @@ void Velox::test()
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
+            ImGui_ImplSDL3_ProcessEvent(&event);
+
             if (event.type == SDL_EVENT_QUIT)
                 g_quitRequested = true;
         }
@@ -38,7 +63,26 @@ void Velox::test()
         // Update game state.
 
         // Render.
+        // Pretty sure this example I stole is just using software rendering (non-gpu).
+        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui_ImplSDL3_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
+        SDL_SetRenderDrawColor(g_renderer, 0, 0, 0, 255);
+        SDL_RenderClear(g_renderer);
+        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), g_renderer);
+        SDL_RenderPresent(g_renderer);
     }
-    
+
+    // Cleanup.
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
+
+    SDL_DestroyRenderer(g_renderer);
+    SDL_DestroyWindow(g_window);
     SDL_Quit();
 }
