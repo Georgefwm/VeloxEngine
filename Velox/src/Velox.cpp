9aca0707a13acfd5e0fa99665bf441a1fdc6a4ff
George Mclachlan <georgemclachlan2@gmail.com>
2025-05-14 17:28:47 +0100

    Changed test file to use GPU rendering

--- Velox/src/Velox.cpp
+++ Velox/src/Velox.cpp
@@ -5,14 +5,15 @@
 
 #include <imgui.h>
 #include <imgui_impl_sdl3.h>
-#include <imgui_impl_sdlrenderer3.h>
+#include <imgui_impl_sdlgpu3.h>
 
 #include <cstdio>
 
 bool g_quitRequested = false;
 
-SDL_Window*   g_window   = nullptr;
-SDL_Renderer* g_renderer = nullptr;
+SDL_Window*    g_window   = nullptr;
+SDL_GPUDevice* g_device   = nullptr;
+SDL_Renderer*  g_renderer = nullptr;
 
 void Velox::test()
 {
@@ -23,29 +24,70 @@ void Velox::test()
     }
 
     SDL_WindowFlags windowFlags = SDL_WINDOW_VULKAN | SDL_WINDOW_HIGH_PIXEL_DENSITY;
-    g_window = SDL_CreateWindow("Velox App", 1280, 720, windowFlags);
+    g_window = SDL_CreateWindow("Velox App", 1920, 1080, windowFlags);
     if (g_window == nullptr)
     {
         printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
         return;
     }
 
-    g_renderer = SDL_CreateRenderer(g_window, nullptr);
-    SDL_SetRenderVSync(g_renderer, 1);
+    SDL_SetWindowPosition(g_window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
+    SDL_ShowWindow(g_window);
 
-    if (!g_renderer) {
-        printf("Error: SDL_CreateRenderer(): %s\n", SDL_GetError());
+    // GM: Set shader format as vulkan (SPIR-V).
+    SDL_GPUShaderFormat shaderFormat = SDL_GPU_SHADERFORMAT_SPIRV;
+
+    g_device = SDL_CreateGPUDevice(shaderFormat, true, nullptr);
+
+    if (g_device == nullptr)
+    {
+        printf("Error: SDL_CreateGPUDevice(): %s\n", SDL_GetError());
+        return;
+    }
+    
+    if (!SDL_ClaimWindowForGPUDevice(g_device, g_window))
+    {
+        printf("Error: SDL_ClaimWindowForGPUDevice(): %s\n", SDL_GetError());
         return;
     }
 
-    SDL_ShowWindow(g_window);
-    
+    // GM: Add option to change SDL_GPU_PRESENTMODE.
+    SDL_SetGPUSwapchainParameters(g_device, g_window, SDL_GPU_SWAPCHAINCOMPOSITION_SDR, SDL_GPU_PRESENTMODE_MAILBOX);
+
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
-    ImGui_ImplSDL3_InitForSDLRenderer(g_window, g_renderer);
-    ImGui_ImplSDLRenderer3_Init(g_renderer);
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplSDL3_InitForSDLGPU(g_window);
+
+    ImGui_ImplSDLGPU3_InitInfo initInfo = {};
+    initInfo.Device = g_device;
+    initInfo.ColorTargetFormat = SDL_GetGPUSwapchainTextureFormat(g_device, g_window);
+    initInfo.MSAASamples = SDL_GPU_SAMPLECOUNT_1;
+
+    ImGui_ImplSDLGPU3_Init(&initInfo);
+
+    // GM: From ImGui example:
+    // Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and stored into a texture when calling ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame below will call.
+    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    //io.Fonts->AddFontDefault();
+    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
+    //IM_ASSERT(font != nullptr);
+    
+    // GM: Need to use our own types.
+    ImVec4 clearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Main loop.
     while (!g_quitRequested)
@@ -59,30 +101,67 @@ void Velox::test()
             if (event.type == SDL_EVENT_QUIT)
                 g_quitRequested = true;
         }
+        
+        // GM: Reduce framerate while window is minimised.
+        if (SDL_GetWindowFlags(g_window) & SDL_WINDOW_MINIMIZED)
+        {
+            SDL_Delay(10);
+        }
 
         // Update game state.
 
         // Render.
-        // Pretty sure this example I stole is just using software rendering (non-gpu).
-        ImGui_ImplSDLRenderer3_NewFrame();
+        ImGui_ImplSDLGPU3_NewFrame();
         ImGui_ImplSDL3_NewFrame();
         ImGui::NewFrame();
 
         ImGui::ShowDemoWindow();
 
         ImGui::Render();
-        SDL_SetRenderDrawColor(g_renderer, 0, 0, 0, 255);
-        SDL_RenderClear(g_renderer);
-        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData(), g_renderer);
-        SDL_RenderPresent(g_renderer);
+
+        ImDrawData* drawData = ImGui::GetDrawData();
+        const bool isMinimised = (drawData->DisplaySize.x <= 0.0f || drawData->DisplaySize.y <= 0.0f);
+
+        SDL_GPUCommandBuffer* commandBuffer = SDL_AcquireGPUCommandBuffer(g_device);
+
+        SDL_GPUTexture* swapchainTexture;
+        SDL_AcquireGPUSwapchainTexture(commandBuffer, g_window, &swapchainTexture, nullptr, nullptr);
+
+        if (swapchainTexture != nullptr && !isMinimised)
+        {
+            // This is mandatory: call ImGui_ImplSDLGPU3_PrepareDrawData() to upload the vertex/index buffer!
+            Imgui_ImplSDLGPU3_PrepareDrawData(drawData, commandBuffer);
+
+            // Setup and start a render pass
+            SDL_GPUColorTargetInfo targetInfo = {};
+            targetInfo.texture     = swapchainTexture;
+            targetInfo.clear_color = SDL_FColor { clearColor.x, clearColor.y, clearColor.z, clearColor.w };
+            targetInfo.load_op     = SDL_GPU_LOADOP_CLEAR;
+            targetInfo.store_op    = SDL_GPU_STOREOP_STORE;
+            targetInfo.mip_level   = 0;
+            targetInfo.cycle       = false;
+            targetInfo.layer_or_depth_plane = 0;
+
+            SDL_GPURenderPass* renderPass = SDL_BeginGPURenderPass(commandBuffer, &targetInfo, 1, nullptr);
+
+            // Render ImGui
+            ImGui_ImplSDLGPU3_RenderDrawData(drawData, commandBuffer, renderPass);
+
+            SDL_EndGPURenderPass(renderPass);
+        }
+
+        SDL_SubmitGPUCommandBuffer(commandBuffer);
     }
 
     // Cleanup.
-    ImGui_ImplSDLRenderer3_Shutdown();
+    SDL_WaitForGPUIdle(g_device);
     ImGui_ImplSDL3_Shutdown();
+    ImGui_ImplSDLGPU3_Shutdown();
     ImGui::DestroyContext();
 
-    SDL_DestroyRenderer(g_renderer);
+    SDL_ReleaseWindowFromGPUDevice(g_device, g_window);
+    SDL_DestroyGPUDevice(g_device);
     SDL_DestroyWindow(g_window);
-    SDL_Quit();
+
+    SDL_Quit();    
 }
