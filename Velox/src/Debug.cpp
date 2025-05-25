f6434dcc1d70093c9ef1bb015668412e11595a72
George Mclachlan <georgemclachlan2@gmail.com>
2025-05-25 15:29:10 +0100

    Improve performance stats

--- Velox/src/Debug.cpp
+++ Velox/src/Debug.cpp
@@ -1,12 +1,13 @@
 #include "Debug.h"
 
+#include "SDL3/SDL_timer.h"
 #include "Util.h"
 #include "imgui.h"
 
-constexpr size_t FRAME_HISTORY_COUNT = 100;
+constexpr size_t FRAME_HISTORY_COUNT = 1000;
 
 static float g_frameTimeHistory[FRAME_HISTORY_COUNT];
-static int   g_currentIndex    = 0;
+static int   g_currentIndex    = FRAME_HISTORY_COUNT;
 static bool  g_capturingPaused = false;
 
 void Velox::DrawPerformanceStats()
@@ -14,6 +15,45 @@ void Velox::DrawPerformanceStats()
     if (!g_capturingPaused)
         Velox::UpdateFrameHistory();
     
+    float count = 0;
+    float max = 0;
+    float min = 10000;
+    float sum = 0;
+    float sumToSecond = 0;
+    int i = g_currentIndex;
+    int fps = 0;
+    while (count <= FRAME_HISTORY_COUNT)
+    {
+        count += 1;
+
+        if (i >= FRAME_HISTORY_COUNT)
+            i = 0;
+
+        float value = g_frameTimeHistory[i];
+
+        i = (i - 1) % FRAME_HISTORY_COUNT;
+
+        if (value <= 0)
+            continue;
+
+        if (sumToSecond <= SDL_MS_PER_SECOND)
+        {
+            sumToSecond += value;
+            fps++;
+        }
+
+        if (value > max)
+            max = value;
+
+        if (value < min)
+            min = value;
+
+        sum += value;
+        
+    }
+
+    float average = sum / FRAME_HISTORY_COUNT;
+
     // Draw UI.
     ImGuiWindowFlags flags = 0;
     
@@ -38,31 +78,32 @@ void Velox::DrawPerformanceStats()
 
     ImGui::Separator();
 
+    ImGui::Text("FrameTimes");
+
     ImGui::Spacing();
-    ImGui::Text("FrameTime (ms): %.0f", g_frameTimeHistory[g_currentIndex]);
+    ImGui::Text("This Frame: %.0f", g_frameTimeHistory[g_currentIndex]);
+
+    ImGui::Spacing();
+    ImGui::Text("Average: %.1f", average);
+
+    ImGui::Spacing();
+    ImGui::Text("Max: %.0f", max);
+
+    ImGui::Spacing();
+    ImGui::Text("Min: %.0f", min);
+
+    ImGui::Separator();
+
+    ImGui::Spacing();
+    ImGui::Text("FPS: %i", fps);
 
     ImGui::Spacing();
     ImGui::Text("Chart: Previous %zu frames", FRAME_HISTORY_COUNT);
 
-    float max, sum;
-    for (int i = 0; i < FRAME_HISTORY_COUNT; i++)
-    {
-        float value = g_frameTimeHistory[i];
-
-        if (value <= 0)
-            continue;
-
-        if (value > max)
-            max = value;
-
-        sum += value;
-    }
-
-    float average = sum / FRAME_HISTORY_COUNT;
     float chartMax = max > 20 ? max * 1.1 : 20;
 
-    ImGui::PlotLines("##Lines", g_frameTimeHistory, IM_ARRAYSIZE(g_frameTimeHistory), g_currentIndex, nullptr,
-            0.0f, chartMax, ImVec2(windowSize.x -20, 100.f));
+    ImGui::PlotLines("##Lines", g_frameTimeHistory, IM_ARRAYSIZE(g_frameTimeHistory),
+            g_currentIndex, nullptr, 0.0f, chartMax, ImVec2(windowSize.x -20, 100.f));
 
     ImGui::PopItemWidth();
     ImGui::End();
@@ -71,7 +112,9 @@ void Velox::DrawPerformanceStats()
 void Velox::UpdateFrameHistory()
 {
     // GM: Wrap around to start.
-    g_currentIndex = (g_currentIndex + 1) % FRAME_HISTORY_COUNT;
+    g_currentIndex -= 1;
+    if (g_currentIndex < 0)
+            g_currentIndex = FRAME_HISTORY_COUNT;
 
     g_frameTimeHistory[g_currentIndex] = Velox::DeltaTimeMS();
 }
