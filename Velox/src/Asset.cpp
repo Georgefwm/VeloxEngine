07cf075c7fef2e8e6115c0f707a52e4c5dfc2e6f
George Mclachlan <georgemclachlan2@gmail.com>
2025-07-06 21:08:07 +0100

    Fix bug where texture would blend with repeating texels

--- Velox/src/Asset.cpp
+++ Velox/src/Asset.cpp
@@ -73,6 +73,9 @@ Velox::Texture* Velox::AssetManager::loadTexture(const char* filepath)
     glTexImage2D(GL_TEXTURE_2D, mipmapLevel, GL_RGBA, surface->w, surface->h, 0, GL_RGBA, GL_UNSIGNED_BYTE, surface->pixels);
     glGenerateMipmap(GL_TEXTURE_2D);
 
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
     SDL_DestroySurface(surface);
 
     char* ptr = g_assetStorage.alloc<char>(strlen(filepath) + 1);
